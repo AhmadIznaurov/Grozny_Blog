1b007b0afc23880822e3472e97724140cf71bd68
AhmadIznaurov <111181823+AhmadIznaurov@users.noreply.github.com>
2023-09-09 21:26:37 +0300

    jali daar du

--- components/Blogs/Blogs.tsx
+++ components/Blogs/Blogs.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import styles from '../../styles/blogs.module.scss';
 import {Blog} from "@/components/Blogs/Blog";
+import { Blogs_dataProps} from "@/app/api/hello/Blogs";
 
-async function getData() {
+
+
+
+
+
+async function getBlogs() {
     const res = await fetch("http://localhost:3000/api/hello");
-
     return await res.json()
 }
 
 export const Blogs: React.FC = async () => {
-    const blogs = await getData()
+    const Blogs_data = await getBlogs()
     return (
         <div className={styles.container}>
-            {
-                blogs.map((item: Blogs_dataProps) => {
-                    return <Blog item={item} key={item.id}/>
-                })
-            }
+                {Blogs_data.map((item: Blogs_dataProps) => {
+                   return <Blog item={item} key={item.id}/>;
+                },
+                )}
         </div>
     );
 }
